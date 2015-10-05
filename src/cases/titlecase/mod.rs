2188bc8cb1f952b38c1bdd80c18096bb38ea6f54
Whatisinternet <joshteeter@gmail.com>
2015-10-05 13:34:11 -0400

    Started work on title case. A lot of failing tests.

---
+++ src/cases/titlecase/mod.rs
@@ -0,0 +1,62 @@
+use std::ascii::*;
+use regex::Regex;
+
+use cases::classcase::is_class_case;
+use cases::camelcase::is_camel_case;
+
+pub fn to_title_case<'a>(non_title_case_string: String) -> String {
+    if is_title_case(non_title_case_string.clone()) {
+        return non_title_case_string
+    } else if
+        is_camel_case(non_title_case_string.clone())
+        || is_class_case(non_title_case_string.clone()){
+        return to_title_from_class_or_camel(non_title_case_string)
+    } else {
+        return to_title_from_snake_or_kebab(non_title_case_string)
+    }
+}
+    fn to_title_from_class_or_camel<'a>(non_title_case_string: String) -> String {
+        let mut result:String = "".to_string();
+        let mut first_character: bool = true;
+        for character in non_title_case_string.chars() {
+            if character == character.to_ascii_uppercase() && !first_character {
+                result = format!("{} {}", result, character);
+            } else if !first_character {
+                result = format!("{}{}", result, character.to_ascii_lowercase());
+            } else {
+                result = format!("{}{}", result, character.to_ascii_uppercase());
+                first_character = false;
+            }
+        }
+        return result
+    }
+
+    fn to_title_from_snake_or_kebab<'a>(non_title_case_string: String) -> String {
+        let mut result:String = "".to_string();
+        let mut first_character: bool = true;
+        for character in non_title_case_string.chars() {
+            if character.to_string() != "_" && character.to_string() != "-" && !first_character {
+                result = format!("{}{}", result, character.to_ascii_lowercase());
+            } else if character.to_string() == "_" || character.to_string() == "-" {
+                result = format!("{} ", result);
+            } else {
+                result = format!("{}{}", result, character.to_ascii_uppercase());
+                first_character = false;
+            }
+        }
+        return result
+    }
+
+pub fn is_title_case<'a>(test_string: String) -> bool{
+    let title_matcher= Regex::new(r"(^[A-Z])([^-|^_]*[a-z]+)").unwrap();
+    let mut is_title_case= false;
+    if title_matcher.is_match(test_string.as_ref())
+        && !is_class_case(test_string.clone()){
+            is_title_case = true;
+        }
+    return is_title_case;
+}
+
+
+#[cfg(test)]
+mod titlecase_test;
