a6d9d787d8c65b9c79825c02889c25805458efed
Whatisinternet <joshteeter@gmail.com>
2015-10-24 14:08:06 -0400

    Started work on speeding up camelCase. Not happy with exactly how it has turned out. Vec and String are very much lacking for split etc.

--- src/cases/camelcase/mod.rs
+++ src/cases/camelcase/mod.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 
 use cases::classcase::is_class_case;
 use cases::snakecase::to_snake_case;
+use cases::uppercase::to_upper_case;
 
 pub fn to_camel_case<'a>(non_camelized_string: String) -> String {
     if is_camel_case(non_camelized_string.clone()) {
@@ -13,21 +14,40 @@ pub fn to_camel_case<'a>(non_camelized_string: String) -> String {
 }
 
     fn to_camel_from_snake<'a>(non_camelized_string: String) -> String{
-        let mut result:String = "".to_string();
-        let mut new_word: bool = false;
-
-        for character in non_camelized_string.chars() {
-            if character.to_string() == "_" {
-                new_word = true;
-            } else if new_word {
-                result = format!("{}{}", result, character.to_ascii_uppercase());
-                new_word = false;
+        let split_string: Vec<&str> = non_camelized_string.split("_").collect();
+        let mut first_word: bool = true;
+        let mut out_string: String = "".to_string();
+        for string in split_string {
+            if first_word {
+                out_string = format!("{}{}", out_string, string);
+                first_word = false;
             } else {
-                result = format!("{}{}", result, character.to_ascii_lowercase());
+                let mut string_chars: Vec<char> = string.chars().collect();
+                let first_char: String = to_upper_case(string.chars().nth(0).unwrap().to_string());
+                string_chars.remove(0);
+                let end_of_word: String = format!("{}", string_chars.iter().cloned().collect::<String>());
+                out_string = format!("{}{}{}", out_string, first_char, end_of_word);
             }
         }
-        return result
+        println!("{}", out_string);
+        return out_string;
     }
+    // fn to_camel_from_snake<'a>(non_camelized_string: String) -> String{
+    //     let mut result:String = "".to_string();
+    //     let mut new_word: bool = false;
+    //
+    //     for character in non_camelized_string.chars() {
+    //         if character.to_string() == "_" {
+    //             new_word = true;
+    //         } else if new_word {
+    //             result = format!("{}{}", result, character.to_ascii_uppercase());
+    //             new_word = false;
+    //         } else {
+    //             result = format!("{}{}", result, character.to_ascii_lowercase());
+    //         }
+    //     }
+    //     return result
+    // }
 
 pub fn is_camel_case<'a>(test_string: String) -> bool{
     let camel_matcher = Regex::new(r"(^|[A-Z])([^-|^_|^ ]*[a-z]+)").unwrap();
