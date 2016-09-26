dd07c6c9882269d00c74fc282ed03c658ae94b9a
Whatisinternet <joshteeter@gmail.com>
2016-09-26 15:44:30 -0400

    Mirrored pluralize with singularize
    This has a huge performance gain over the previous implementation of
    to_singular. It uses the recommendations put forward in
    https://github.com/rust-lang-nursery/regex/blob/master/PERFORMANCE.md
    and macros.

--- src/string/singularize/mod.rs
+++ src/string/singularize/mod.rs
@@ -1,12 +1,6 @@
 use regex::Regex;
 use regex::Captures;
 use string::constants::UNACCONTABLE_WORDS;
-
-const SINGULAR_RULES: [&'static str; 27] = ["ews", "", "um", "sis", "sis", "fe", "", "", "f", "y",
-                                            "eries", "ovie", "", "ouse", "", "", "is", "xis",
-                                            "us", "", "", "ex", "ix", "", "", "", ""];
-
-
 /// Converts a `String` to singularized `String`
 ///
 /// #Examples
@@ -59,151 +53,111 @@ const SINGULAR_RULES: [&'static str; 27] = ["ews", "", "um", "sis", "sis", "fe",
 ///
 /// ```
 ///
+macro_rules! special_cases{
+    ($s:ident, $($singular: expr => $plural:expr), *) => {
+        match &$s[..] {
+            $(
+                $singular => {
+                    return $plural.to_string();
+                },
+            )*
+            _ => ()
+        }
+    }
+}
 pub fn to_singular(non_singular_string: String) -> String {
     if UNACCONTABLE_WORDS.contains(&non_singular_string.as_ref()) {
         non_singular_string
     } else {
-        let safe_out_string: String = non_singular_string.to_string();
-        let maybe_regex_match: Option<String> = singularize_string(non_singular_string);
-        maybe_regex_match.unwrap_or(safe_out_string)
-    }
-}
-
-fn singularize_string(non_singular_string: String) -> Option<String> {
-    let singular_regexes: [Regex; 27] = singular_rules_regexes();
-    for singular_index in 0..singular_regexes.len() {
-        let maybe_match: Option<String> =
-            singularize_string_from_capture_groups_if_match(non_singular_string.to_string(),
-                                                            singular_index);
-
-        if maybe_match.is_some() {
-            return Some(maybe_match.unwrap_or(non_singular_string));
+        special_cases![non_singular_string,
+            "oxen" => "ox",
+            "men" => "man",
+            "women" => "woman",
+            "dice" => "die",
+            "yeses" => "yes",
+            "feet" => "foot",
+            "eaves" => "eave",
+            "geese" => "goose",
+            "teeth" => "tooth",
+            "quizzes" => "quiz"
+        ];
+        for &(ref rule, replace) in RULES.iter().rev() {
+            if let Some(c) = rule.captures(&non_singular_string) {
+                if let Some(c) = c.at(1) {
+                    return format!("{}{}", c, replace);
+                }
+            }
         }
-    }
-    return None;
-}
 
-fn singularize_string_from_capture_groups_if_match(non_singular_string: String,
-                                                   singular_index: usize)
-                                                   -> Option<String> {
-    let singular_regexes: [Regex; 27] = singular_rules_regexes();
-
-    if singular_regexes[singular_index].is_match(&non_singular_string) {
-
-        let cap: Captures = singular_regexes[singular_index]
-            .captures(&non_singular_string)
-            .unwrap();
-
-        Some(singularize_string_from_capture_groups(cap, singular_index))
-    } else {
-        None
+        format!("{}", non_singular_string)
     }
 }
 
-fn singularize_string_from_capture_groups(capture_groups: Captures,
-                                          singular_index: usize)
-                                          -> String {
-    if capture_groups.len() > 2 {
 
-        return format!("{}{}{}",
-                       capture_groups.at(1).unwrap_or(""),
-                       capture_groups.at(2).unwrap_or(""),
-                       SINGULAR_RULES[singular_index]);
-    } else {
-
-        return format!("{}{}",
-                       capture_groups.at(1).unwrap_or(""),
-                       SINGULAR_RULES[singular_index]);
+macro_rules! add_rule{
+    ($r:ident, $rule:expr => $replace:expr) => {
+        $r.push((Regex::new($rule).unwrap(), $replace));
     }
 }
 
-fn singular_rules_regexes() -> [Regex; 27] {
-    [Regex::new(r"(n)ews$").unwrap(),
-     Regex::new(r"(\w*)(o)es$").unwrap(),
-     Regex::new(r"(\w*)([ti])a$").unwrap(),
-     Regex::new(r"((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$").unwrap(),
-     Regex::new(r"(^analy)(sis|ses)$").unwrap(),
-     Regex::new(r"(\w*)([^f])ves$").unwrap(),
-     Regex::new(r"(\w*)(hive)s$").unwrap(),
-     Regex::new(r"(\w*)(tive)s$").unwrap(),
-     Regex::new(r"(\w*)([lr])ves$").unwrap(),
-     Regex::new(r"(\w*)([^aeiouy]|qu)ies$").unwrap(),
-     Regex::new(r"(s)eries$").unwrap(),
-     Regex::new(r"(m)ovies$").unwrap(),
-     Regex::new(r"(\w*)(x|ch|ss|sh)es$").unwrap(),
-     Regex::new(r"(m|l)ice$").unwrap(),
-     Regex::new(r"(bus)(es)?$").unwrap(),
-     Regex::new(r"(shoe)s$").unwrap(),
-     Regex::new(r"(cris|test)(is|es)$").unwrap(),
-     Regex::new(r"^(a)x[ie]s$").unwrap(),
-     Regex::new(r"(octop|vir)(us|i)$").unwrap(),
-     Regex::new(r"(alias|status)(es)?$").unwrap(),
-     Regex::new(r"^(ox)en").unwrap(),
-     Regex::new(r"(vert|ind)ices$").unwrap(),
-     Regex::new(r"(matr)ices$").unwrap(),
-     Regex::new(r"(quiz)zes$").unwrap(),
-     Regex::new(r"(database)s$").unwrap(),
-     Regex::new(r"(\w*)s$").unwrap(),
-     Regex::new(r"(\w*)(ss)$").unwrap()]
+macro_rules! rules{
+    ($r:ident; $($rule:expr => $replace:expr), *) => {
+        $(
+            add_rule!{$r, $rule => $replace}
+        )*
+    }
+}
 
+
+lazy_static!{
+    static ref RULES: Vec<(Regex, &'static str)> = {
+    let mut r = Vec::new();
+    rules![r;
+     r"(n)ews$" => "ews",
+     r"(\w*)(o)es$" => "",
+     r"(\w*)([ti])a$" => "um",
+     r"((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$" => "sis",
+     r"(^analy)(sis|ses)$" => "sis",
+     r"(\w*)([^f])ves$" => "fe",
+     r"(\w*)(hive)s$" => "",
+     r"(\w*)(tive)s$" => "",
+     r"(\w*)([lr])ves$" => "f",
+     r"(\w*)([^aeiouy]|qu)ies$" => "y",
+     r"(s)eries$" => "eries",
+     r"(m)ovies$" => "ovie",
+     r"(\w*)(x|ch|ss|sh)es$" => "",
+     r"(m|l)ice$" => "ouse",
+     r"(bus)(es)?$" => "",
+     r"(shoe)s$" => "",
+     r"(cris|test)(is|es)$" => "is",
+     r"^(a)x[ie]s$" => "xis",
+     r"(octop|vir)(us|i)$" => "us",
+     r"(alias|status)(es)?$" => "",
+     r"^(ox)en" => "",
+     r"(vert|ind)ices$" => "ex",
+     r"(matr)ices$" => "ix",
+     r"(quiz)zes$" => "",
+     r"(database)s$" => "",
+     r"(\w*)s$" => "",
+     r"(\w*)(ss)$" => ""
+         ];
+     r
+    };
 }
 
 #[test]
 fn singularize_string_if_a_regex_will_match() {
-    let expected_string: String = "oxen".to_string();
-    let asserted_string: Option<String> = singularize_string(expected_string);
-
-    assert!(asserted_string.is_some());
+    let expected_string: String = "ox".to_string();
+    let asserted_string: String = to_singular("oxen".to_string());
+    assert!(expected_string == asserted_string);
 
 }
 
 #[test]
 fn singularize_string_returns_none_option_if_no_match() {
     let expected_string: String = "bacon".to_string();
-    let asserted_string: Option<String> = singularize_string(expected_string);
+    let asserted_string: String = to_singular("bacon".to_string());
 
-    assert!(asserted_string.is_none());
-
-}
-
-#[test]
-fn singularize_string_from_capture_groups_if_match_should_return_a_string_if_a_match() {
-    let expected_string: String = "oxen".to_string();
-    let singular_index: usize = 20;
-    let asserted_string: Option<String> =
-        singularize_string_from_capture_groups_if_match(expected_string, singular_index);
-
-    assert!(asserted_string.is_some());
-
-}
-
-#[test]
-fn singularize_string_from_capture_groups_if_match_should_return_an_option_none_if_no_match() {
-    let expected_string: String = "bacon".to_string();
-    let singular_index: usize = 20;
-    let asserted_string: Option<String> =
-        singularize_string_from_capture_groups_if_match(expected_string, singular_index);
-
-    assert!(asserted_string.is_none());
-
-}
-
-#[test]
-fn singularize_string_from_capture_groups_should_return_a_singularized_string() {
-    let expected_string: String = "ox".to_string();
-    let test_string: String = "oxen".to_string();
-    let singular_regexes: [Regex; 27] = singular_rules_regexes();
-    let singular_index: usize = 20;
-    let cap: Captures = singular_regexes[singular_index]
-        .captures(&test_string)
-        .unwrap();
-    let asserted_string: String = singularize_string_from_capture_groups(cap, singular_index);
     assert!(expected_string == asserted_string);
 
-}
-
-#[test]
-fn singular_rules_regexes_should_return_27_regex() {
-    let regexes: [Regex; 27] = singular_rules_regexes();
-    assert!(regexes.len() == 27);
 }
