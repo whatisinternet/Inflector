e5dc3886b69c749ed314c6634ff370b7961fb09c
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-02 19:06:57 -0400

    add other failing test

--- src/cases/classcase/mod.rs
+++ src/cases/classcase/mod.rs
@@ -344,6 +344,13 @@ mod tests {
     }
 
     #[test]
+    fn has_special_singular_form_2() {
+        let convertable_string: String = "my_leaves".to_owned();
+        let expected: String = "MyLeaf".to_owned();
+        assert_eq!(to_class_case(&convertable_string), expected)
+    }
+
+    #[test]
     fn is_correct_from_class_case() {
         let convertable_string: String = "fooBar".to_owned();
         assert_eq!(is_class_case(&convertable_string), false)
